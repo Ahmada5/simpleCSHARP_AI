d709d20ddd526c8f539eeefb8d917bd6662cca0f
ahmada <achmad.ahmada@gmail.com>
2022-10-30 21:35:51 +0700

    new upload

---
+++ AI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI2 : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent agent;
+    public float radius;
+    Rigidbody rb;
+    Animator animator;
+
+
+    private void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        if (!agent.hasPath)
+        {
+            agent.SetDestination(GETPOINT.Instance.GetRandomPoint(transform, radius));
+            animator.SetBool("walk", false);
+
+            Debug.Log("tidakjalan");
+        }
+        else if (agent.hasPath)
+        {
+            animator.SetBool("walk", true);
+ 
+            Debug.Log("jalan");
+        }
+    }
+
+
+
+
+}
